29dc99fdf70c0f1c3bd70c89b11bbb48c4de014f
Latchezar Kostov <latch.kostov@gmail.com>
2022-09-18 09:44:54 -0500

    initial commit

---
+++ src/lib/DayCard.svelte
@@ -0,0 +1,11 @@
+<div class="day-card-container">
+
+</div>
+
+<style>
+    .day-card-container {
+        height: 100px;
+        width: 100px;
+        border: 1px red solid;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import DayCard from '../lib/DayCard.svelte';
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<div class="w-full h-screen flex flex-col">
+	<h1 class="header text-center text-2xl">{data.title}</h1>
+	<div class="body">
+		<div class="h-full flex justify-between items-center px-5">
+			<DayCard />
+			<DayCard />
+			<DayCard />
+			<DayCard />
+			<DayCard />
+		</div>
+	</div>
+</div>
+
+<style>
+	.header {
+		border: 1px red solid;
+		background-color: skyblue;
+		padding-block: 10px;
+	}
+	.body {
+		border: 1px red solid;
+		background-color: skyblue;
+		flex-grow: 1;
+	}
+</style>
